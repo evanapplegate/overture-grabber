f7e5c0ec8e3c960a05e99185e4fe8c2fcdde4acd
evanapplegate <evanapplegate@users.noreply.github.com>
2024-05-29 23:02:23 -0400

    Changes

--- overture_grabber.py
+++ overture_grabber.py
@@ -1,7 +1,7 @@
 from flask import Flask, request, send_file, render_template, jsonify
 import subprocess
-import os
 import tempfile
+import os
 
 app = Flask(__name__)
 
@@ -34,4 +34,5 @@ def download_geojson():
         return {"error": "Failed to download geojson"}, 500
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(debug=True, host='0.0.0.0', port=port)
